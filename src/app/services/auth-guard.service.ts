d2ad1de64d5ce06004ec89f56d2ea879e19c6f80
Ahmed Samir <ahmedsamir@Ahmeds-MacBook-Pro.local>
2018-11-24 00:35:53 +0200

    version X2.7

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from "@angular/router";
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 import { AuthService } from './auth.service';
 import { map } from 'rxjs/operators';
 
@@ -10,11 +10,11 @@ export class AuthGuard implements CanActivate {
 
   constructor(private authService: AuthService, private router: Router) { }
 
-  canActivate() {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     return this.authService.user$.pipe(map(user => {
       if(user) return true;
 
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
       return false;
     }));
   }
